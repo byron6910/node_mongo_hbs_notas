c22ed01660e1e2b9e52131b713a8d7dec4e5a00f
byron6910 <bralvarezm@gmail.com>
2019-06-12 21:51:25 -0500

    hasta el 2h40

---
+++ src/config/passport.js
@@ -0,0 +1,7 @@
+const passport =require('passport');
+const LocalStrategy=require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+    usernameField:'email',
+    passwordField:'password'
+}));
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,5 @@
 const router=require('express').Router();
+const User=require('../models/users');
 
 router.get('/users/signin',(req,res)=>{
     res.render('users/signin');
@@ -7,6 +8,37 @@ router.get('/users/signin',(req,res)=>{
 router.get('/users/signup',(req,res)=>{
     res.render('users/signup');
 
+});
+
+router.post('/users/signup',async (req,res)=>{
+    const {name,email,password,confirm_password}=req.body;
+    let errors=[];
+    if(name.length<=0){
+        errors.push({text:'Ingrese Nombre'});
+    }
+    if(password!=confirm_password){
+        errors.push({text:'Error de contrasenas, no coinciden'})
+    }
+    if(password.length<4){
+        errors.push({text:'La contrasena debe ser minimo 4 caracteres'})
+    }
+    if(errors.length>0){
+        res.render('users/signup',{errors,name,email,password,confirm_password})
+    }else
+    { 
+        const emailUser=await User.findOne({email:email});
+        if(emailUser){
+            req.flash('error_msg','El email: '+emailUser.email+'esta en uso');
+            res.redirect('/users/signup');
+        }else{
+        const newUser=new User({name,email,password});
+        newUser.password=await newUser.encryptPassword(password);
+        await newUser.save();
+        req.flash('success_msg','Tu estas registrado');
+        res.redirect('/users/signin');
+        } 
+
+    }
 });
 
 
